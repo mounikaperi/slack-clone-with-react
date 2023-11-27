cbde823e0d145e635e15a0f6eda608f9fe22f1f1
Sai Mounika Peri <saimounikaperi@gmail.com>
2023-11-27 18:15:50 +0530

    Reset Password page

--- src/components/Login/ForgotPassword.js
+++ src/components/Login/ForgotPassword.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ForgotPassword() {
   return (
-    <div className="container">
-      <h1>Password Reset</h1>
-      <p>To reset your password, enter the email address you use to sign in to {"workspaceName"}</p>
-      <input type="text" placeholder='you@example.com'></input>
-      <button onClick={"loginToWorkspace"} className="workspace__button">
-        <p>Get Reset Link</p>
-      </button>
-    </div>
+    <>
+      <div className="header">
+        <img src="https://tse3.mm.bing.net/th?id=OIP.VNTO9A4Ya48o-0GRgCPkogHaEK&pid=Api&P=0&h=180" alt="slack" />
+        <div className="header__right">
+          <Link to="#">Product</Link>
+          <Link to="#">Pricing</Link>
+          <button onClick={"loginToWorkspace"} className="workspace__button">
+            <p>Sign In</p>
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Password Reset</h1>
+        <p>To reset your password, enter the email address you use to sign in to {"workspaceName"}</p>
+        <input type="text" placeholder='you@example.com'></input>
+        <button onClick={"loginToWorkspace"} className="workspace__button">
+          <p>Get Reset Link</p>
+        </button>
+      </div>
+    </>
   )
 }
 
